2571338bb99873f2bea6d1342977a001ec3421b7
taochen <Tao.CA.Chen@batw.bmwgroup.com>
2025-10-15 13:16:48 +0800

    feat: add prompts for api_server_type and api_version in profile configuration

--- occ/configuration/profile_config.py
+++ occ/configuration/profile_config.py
@@ -9,13 +9,21 @@ def add_default_profile():
                            default=config.get_default_env("api_key"), type=str)
     config.set_env('default', 'api_key', api_key)
 
-    api_key = click.prompt(logger.style('\n[default] Your api_base_url(Optional) '),
+    api_base_url = click.prompt(logger.style('\n[default] Your api_base_url(Optional) '),
                            default=config.get_default_env("api_base_url"), type=str)
-    config.set_env('default', 'api_base_url', api_key)
+    config.set_env('default', 'api_base_url', api_base_url)
 
     limit_history = click.prompt(logger.style('\n[default] Your default limit_history '),
                                  default=config.get_default_env("limit_history"), type=str)
     config.set_env('default', 'limit_history', limit_history)
+
+    api_server_type = click.prompt(logger.style('\n[default] Your default api_server_type '),
+                                 default=config.get_default_env("api_server_type"), type=str)
+    config.set_env('default', 'api_server_type', api_server_type)
+
+    api_version = click.prompt(logger.style('\n[default] Your default api_version '),
+                                 default=config.get_default_env("api_version"), type=str)
+    config.set_env('default', 'api_version', api_version)
 
 
 def input_config_vars(profile_name, key_name, is_default_exist):
@@ -34,3 +42,5 @@ def add_profile(profile_name):
     input_config_vars(profile_name, 'api_key', False)
     input_config_vars(profile_name, 'api_base_url', False)
     input_config_vars(profile_name, 'limit_history', False)
+    input_config_vars(profile_name, 'api_server_type', False)
+    input_config_vars(profile_name, 'api_version', False)
